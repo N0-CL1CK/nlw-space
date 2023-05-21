75b335ff60a04bc8e37e762b2932788cd135ef92
N0-CL1CK <luis.gsn@outlook.com>
2023-05-20 21:29:33 -0400

    back-end aula 2/5 concluído

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,57 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors';
 import { memoryRoutes } from './routes/memory.routes';
 
 class Application {
   private app = fastify();
 
-  private buildRoutes(): void {
-    this.app.register(memoryRoutes, { prefix: '/memories' });
+  private applySettings(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      try {
+        this.app.register(cors, { origin: true });
+
+        resolve();
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    });
   }
 
-  private listen(): void {
-    this.app
-      .listen({ port: 8080 })
-      .then(() =>
-        console.log(`🚀 HTTP server running on http://localhost:8080/`),
-      );
+  private buildRoutes(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      try {
+        this.app.register(memoryRoutes, { prefix: '/memories' });
+
+        resolve();
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    });
   }
 
-  public load(): void {
-    this.buildRoutes();
-    this.listen();
+  private listen(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      try {
+        this.app
+          .listen({ port: 8080 })
+          .then(() =>
+            console.log(`🚀 HTTP server running on http://localhost:8080/`),
+          );
+
+        resolve();
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    });
+  }
+
+  public async load(): Promise<void> {
+    await this.applySettings();
+    await this.buildRoutes();
+    await this.listen();
   }
 }
 // add a comment
