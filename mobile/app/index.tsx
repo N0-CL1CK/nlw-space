2af282985807a1095f9b6487b119d273fc8f57a4
N0-CL1CK <luis.gsn@outlook.com>
2023-05-24 21:04:26 -0400

    finalizado o mobile da aula 3/5, feito a parte de autenticação com o github e metade do roteamento para a tela de logado, falta implementar a persistencia de login

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,20 +1,55 @@
+import { useEffect } from 'react'
+import { styled } from 'nativewind'
+import { useRouter } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import * as SecureStore from 'expo-secure-store'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 
-import bgBlur from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
+import bgBlur from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/feba3386659a60d23873',
+}
+
 export default function App() {
+  const router = useRouter()
+
+  const [, response, signIn] = useAuthRequest(
+    {
+      clientId: 'feba3386659a60d23873',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleOAuth(code: string) {
+    const response = await api.post('/auth/register', { code })
+    await SecureStore.setItemAsync('token', response.data.token)
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') handleOAuth(response.params.code)
+  }, [response])
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
@@ -47,6 +82,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.5}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signIn()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
